fc3ba8f062c1fe19bb55747673ba3b69745a555b
Nademtis <mathias.99.madsen@gmail.com>
2025-10-15 22:12:14 +0200

    wip on cutscene - somewhat works

--- scenes/cutscene/cutscene.gd
+++ scenes/cutscene/cutscene.gd
@@ -1,6 +1,7 @@
 extends Node
 #cutscene unlock mask
 @onready var player: Player = $"../ysort/player"
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_start_cutscene_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
@@ -8,4 +9,19 @@ func _on_start_cutscene_area_2d_area_entered(area: Area2D) -> void:
 		player.velocity = Vector2.ZERO
 		player.can_move = false
 		player.animated_sprite_2d.play("stand_still")
-	pass # Replace with function body.
+		
+		await get_tree().create_timer(1).timeout
+		animation_player.play("create_mask_cutscene")
+		
+func walk() -> void:
+	player.animated_sprite_2d.play("walk_down")
+
+func stop() -> void:
+	player.animated_sprite_2d.play("stand_still")
+
+func interact() -> void:
+	player.animated_sprite_2d.play("interact")
+	
+func walk_right() -> void:
+	player.animated_sprite_2d.play("walk_right")
+	
