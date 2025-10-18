3e3180c629d4ca02b6e11b10b774c1c78481418e
Nademtis <mathias.99.madsen@gmail.com>
2025-10-18 18:49:18 +0200

    2 more levels

--- scenes/audio_levelup.gd
+++ scenes/audio_levelup.gd
@@ -3,13 +3,14 @@ extends Area2D
 
 @export var level_up_music : bool = false
 #@export var level_up_ambience : bool = false
-
+@export var indented_level : int
 
 
 
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
+		
 		Events.emit_signal("level_up_music", level_up_music)
 		#Events.emit_signal("level_up_ambience", level_up_ambience)
 		set_deferred("monitoring", false)
