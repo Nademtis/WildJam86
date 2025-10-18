59b21f98386979c08f02f44fbc4b63086378b1f9
Nademtis <mathias.99.madsen@gmail.com>
2025-10-18 13:04:15 +0200

    wip on mask use cutscene level

--- scripts/use_mask_cutscene.gd
+++ scripts/use_mask_cutscene.gd
@@ -1,24 +1,23 @@
 extends Node
-#cutscene unlock mask
-@onready var player: Player = $"../ysort/player"
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var craftingSFX: AudioStreamPlayer2D
 
-func _ready() -> void:
-	if has_node("CraftingSFX"):
-		craftingSFX = $CraftingSFX
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+@onready var blackborder_upper: ColorRect = $blackborderUpper
+@onready var blackborder_lower: ColorRect = $blackborderLower
+
+@onready var player: Player = $"../ysort/player"
 
 func _on_start_cutscene_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
-		#print("start cutscene")
+		print("start cutscene")
 		player.velocity = Vector2.ZERO
 		player.can_move = false
 		player.step_playlist.stop()
 		player.animated_sprite_2d.play("stand_still")
 		
 		await get_tree().create_timer(1).timeout
-		animation_player.play("create_mask_cutscene")
-		
+		animation_player.play("use_mask_cutscene")
+
 func walk_down() -> void:
 	player.step_playlist.play()
 	player.animated_sprite_2d.play("walk_down")
@@ -27,14 +26,10 @@ func stop() -> void:
 	player.step_playlist.stop()
 	player.animated_sprite_2d.play("stand_still")
 
-func interact() -> void:
-	player.step_playlist.stop()
-	craftingSFX.play()
-	player.animated_sprite_2d.play("interact")
-	
 func walk_right() -> void:
 	player.step_playlist.play()
 	player.animated_sprite_2d.play("walk_right")
 	
-func look_up() -> void:
-	player.animated_sprite_2d.play("look_up")
+func mask_walk_right() -> void:
+	player.step_playlist.play()
+	player.animated_sprite_2d.play("mask_walk_right")
